e5e86e16bbaa21e34fa0a3465ddcce37a8e1f624
Alan <Alankiris@gmail.com>
2022-01-03 03:09:57 +0200

    Final commit

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+    let discordButton = document.getElementById("discordButton");
+    discordButton.onclick = function(){
+        navigator.clipboard.writeText('Kiri#3447');
+        alert("Copied the text: " + 'Kiri#3447');
+    };
